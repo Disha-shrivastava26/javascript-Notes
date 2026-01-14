c1b81b7d36b143bf2f11b5dc2e5d85f5ef39acf5
Disha shrivastava <shrivastavadisha26@gmail.com>
2026-01-14 16:28:37 +0530

    Create conversionandcoercion.js

---
+++ fundamental part1/conversionandcoercion.js
@@ -0,0 +1,22 @@
+// conversion
+
+const age1 = "1991";
+
+console.log(Number(age1));
+
+const age2 = 21;
+console.log(String(age2));
+
+// type coercion
+
+// +
+console.log(2 + 4 + 5 + "9"); //119
+// -
+console.log("99" - "11"); //88
+
+// *
+console.log("99" * "11"); //1089
+console.log(99 * "11"); //convert string to number
+
+//  /
+console.log("99" / "11"); // convert the string to number
