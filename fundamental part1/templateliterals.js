94fdc02c1dd875fe5dc1a7c1eb1374c4f3581530
Disha shrivastava <shrivastavadisha26@gmail.com>
2026-01-14 15:35:38 +0530

    Add template literals example in templateliterals.js

---
+++ fundamental part1/templateliterals.js
@@ -0,0 +1,13 @@
+const age = 21;
+const firstName = "disha";
+const job = "student";
+
+console.log(`Hey`);
+console.log(`I'm ${firstName} a ${age} years old ${job}!`);
+
+// to write full strings
+console.log(`you can write whole string`);
+// to write muiltple strings
+
+console.log(`this is the case
+  we can write multiple strings along side`);
