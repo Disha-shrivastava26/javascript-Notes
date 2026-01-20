f5a39d7af56bbab8091c83a30aa51dfa9f9ec1e8
Disha shrivastava <shrivastavadisha26@gmail.com>
2026-01-21 00:57:19 +0530

    Add datastructureArray.js with array examples

---
+++ fundamentalpart2/datastructureArray.js
@@ -0,0 +1,27 @@
+`use strict`
+
+// data structure in js
+// Arrays
+
+// what is the purpose => to store data
+// in js array can store Both homogenous and non-homengous data
+
+//
+const names1 = ["disha", "mimansa", "khushi"];
+
+//
+
+const friends = new Array("disha", "mimansa", "khushi");
+
+// we can manpulate the data of array cannot the whole array
+// const keyword allow the manipulation of data for the non-primitive data types
+
+console.log(names1);
+console.log(friends);
+
+function job() {
+  console.log("i am engineering student,learning frontend development");
+}
+
+const id = ["disha", "shrivastava", 2026 - 2004, job(), friends];
+console.log(id);
