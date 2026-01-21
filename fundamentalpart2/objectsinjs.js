1c1bba8209a22350119775eef08ba0913f7b6350
Disha shrivastava <shrivastavadisha26@gmail.com>
2026-01-22 00:53:18 +0530

    Create objectsinjs.js

---
+++ fundamentalpart2/objectsinjs.js
@@ -0,0 +1,33 @@
+`use strict`
+
+// objects in javascript
+
+// object in js is to data which is not structured
+// means they can be accessible at any order
+
+// create an object
+
+const disha = {
+  firstName: "Disha",
+  lastName: "shrivastava",
+  hobby: "dance",
+  job: "developer",
+};
+
+// how to access the objects property
+
+// dot notation
+// bracket notation
+
+console.log(disha.lastName); // it will search the property with same name
+
+console.log(disha["lastName"]); // it will the compute the value hold by the expression
+
+// how to add the value
+
+disha.location = "indore";
+disha[location] = "indore";
+
+console.log(
+  `${disha.firstName} has hobby ${disha.hobby} but she is in ${disha.location}`,
+);
