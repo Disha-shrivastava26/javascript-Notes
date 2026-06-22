c89d0d9e16820254233b77689caaf940e41a2788
Disha shrivastava <shrivastavadisha26@gmail.com>
2026-06-22 23:46:19 +0530

    Update stickyNav.js

--- DOM/stickyNav.js
+++ DOM/stickyNav.js
@@ -14,3 +14,34 @@ window.addEventListener('scroll', function () {
     nav.classList.remove('sticky');
   }
 });
+
+//usign IntersectionObserver APIs
+
+//sticky nav using IntersectionObserver
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  //console.log(entry);
+
+  //agar intersection==false tab class add kar do baki remove kar do
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+
+const stickyNavoptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, stickyNavoptions);
+headerObserver.observe(header);
+
